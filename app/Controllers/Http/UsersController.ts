98e0107b5d3eb4132baa0fd6197aa7ae5aef94b4
GabriielB <gabrielbraga1953@hotmail.com>
2023-10-13 00:38:11 -0300

    add userValidators, BadrequestException, Factories

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,9 +1,23 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import BadRequestException from 'App/Exceptions/BadRequestException'
 import User from 'App/Models/User'
+import CreateUserValidator from 'App/Validators/CreateUserValidator'
 
 export default class UsersController {
   public async store({ request, response }: HttpContextContract) {
-    const userPayload = request.only(['email', 'username', 'password', 'avatar'])
+    const userPayload = await request.validate(CreateUserValidator)
+
+    const userByEmail = await User.findBy('email', userPayload.email)
+    const userByUsername = await User.findBy('username', userPayload.username)
+
+    if (userByEmail) {
+      throw new BadRequestException('Email ja existe!', 409)
+    }
+
+    if (userByUsername) {
+      throw new BadRequestException('Username ja existe!', 409)
+    }
+
     const user = await User.create(userPayload)
     return response.created({ user })
   }
--- test/users/users.spec.ts
+++ test/users/users.spec.ts
@@ -1,3 +1,6 @@
+import Database from '@ioc:Adonis/Lucid/Database'
+import User from 'App/Models/User'
+import { UserFactory } from 'Database/factories'
 import test from 'japa'
 import supertest from 'supertest'
 
@@ -5,9 +8,9 @@ import supertest from 'supertest'
 const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
 
 // Grupo de testes nomeado de User
-test.group('User', () => {
+test.group('User', (group) => {
   // Teste Unitario esperando o código 201 da requisição POST
-  test.only('Deve criar um usuário', async (assert) => {
+  test('Deve criar um usuário', async (assert) => {
     const userPayload = {
       email: 'test@gmail.com',
       username: 'teste',
@@ -22,5 +25,37 @@ test.group('User', () => {
     assert.equal(body.user.username, userPayload.username)
     assert.notExists(body.user.password, userPayload.password)
     assert.equal(body.user.avatar, userPayload.avatar)
+  })
+  test('Deve retornar o 409 o email ja estiver em uso', async () => {
+    // Utilizando as Factorys para selecionar o email
+    const { email } = await UserFactory.create()
+    // selecionando o body criando uma requisicao para enviar o email da factory e enviar username e senha como teste
+    const { body } = await supertest(BASE_URL)
+      .post('/users')
+      .send({ email: email, username: 'teste', password: 'teste' })
+      .expect(409)
+  })
+
+  test('Deve retornar o 409 o username ja existe', async () => {
+    const { username } = await UserFactory.create()
+    const { body } = await supertest(BASE_URL)
+      .post('/users')
+      .send({ email: 'teste', username, password: 'teste' })
+      .expect(409)
+  })
+
+  test('Deve retornar 422 quando eu não informar os dados obrigatorios', async (assert) => {
+    const { body } = await supertest(BASE_URL).post('/users').send({}).expect(422)
+    assert.equal(body.code, 'BAD_REQUEST')
+    assert.equal(body.status, 422)
+  })
+  // Utilizando hooks para executar comandos antes e dps de cada teste
+  group.beforeEach(async () => {
+    // rodando uma transação global para os dados não serem persistidos a cada vez que o controller rodar
+    await Database.beginGlobalTransaction()
+  })
+
+  group.afterEach(async () => {
+    await Database.rollbackGlobalTransaction()
   })
 })
